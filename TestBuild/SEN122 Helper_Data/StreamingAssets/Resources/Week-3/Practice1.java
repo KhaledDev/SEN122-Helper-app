4d570a33499cb0cb1c07b65994948f15ff83eaa1
KhaledDev <56503740+KhaledDev@users.noreply.github.com>
2024-04-01 13:46:47 +0300

    Removed testbuild folder....
    I forgor :skull:

--- TestBuild/SEN122 Helper_Data/StreamingAssets/Resources/Week-3/Practice1.java
+++
@@ -1,9 +0,0 @@
-// Natural numbers from 1 to 100 (using a while loop)
-public class Practice1 {
-    public static void main(String[] args) {
-        int i = 1;
-
-        while (i<=100)
-            System.out.println(i++);
-    }
-}
